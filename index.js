2e4924fcf19f081508bde69f4e7ab9707d07c606
Ari <aritheriver@gmail.com>
2023-01-14 15:10:47 -0800

    finished

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = titleCased.map((tutorials) => {
+//   return tutorials[0].toUpperCase() + tutorials.substring(1);
+// });
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((words) => {
+    const word = words.split(' ');
+    const captializedWord = word.map((letter) => { 
+      console.log(letter[0].toUpperCase() + letter.substring(1));
+      return letter[0].toUpperCase() + letter.substring(1);
+    })
+  return captializedWord.join(' ')})}
+
+console.log(titleCased);
+// tutorials.map(word)
